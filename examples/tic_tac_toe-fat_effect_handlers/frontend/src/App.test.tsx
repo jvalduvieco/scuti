3636916fc7b014b2b6cf9541ae21ff4709ddac5c
jvalduvieco <jvalduvieco@gmail.com>
2022-10-26 10:15:38 +0200

    run frontend tests upgrade some deps

--- examples/tic_tac_toe-fat_effect_handlers/frontend/src/App.test.tsx
+++ examples/tic_tac_toe-fat_effect_handlers/frontend/src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, screen} from '@testing-library/react';
+import {Lobby} from "./components/Lobby";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders lobby screen', () => {
+  render(<Lobby onNewGame={() => null}/>);
+  const linkElement = screen.getByText(/Welcome to tic tac toe!/i);
   expect(linkElement).toBeInTheDocument();
 });
