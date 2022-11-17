14dbe8779c988ec4968ed35b757e2f984ea069fd
jvalduvieco <jvalduvieco@gmail.com>
2022-11-17 12:55:03 +0100

    Threads show names in top

--- mani/infrastructure/domain/cqrs/bus/asynchronous_bus_runner.py
+++ mani/infrastructure/domain/cqrs/bus/asynchronous_bus_runner.py
@@ -1,5 +1,6 @@
 import threading
 
+import pyprctl
 from injector import inject
 
 from mani.infrastructure.domain.cqrs.bus.asynchronous_bus import AsynchronousBus
@@ -11,6 +12,7 @@ logger = get_logger(__name__)
 @inject
 def asynchronous_bus_runner(bus: AsynchronousBus):
     logger.info("Sequential bus runner starting...")
+    pyprctl.set_name("Sequential bus runner")
     self = threading.current_thread()
     while getattr(self, "should_be_running", True):
         bus.drain()
